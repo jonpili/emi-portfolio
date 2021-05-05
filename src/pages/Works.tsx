5d1a3d09effd9a5610059dfaa639f0a7d3ef6d49
JoeTakasuna <jonpili99@gmail.com>
2021-05-05 17:22:19 +0900

    make works style loosely

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import './Works.scss';
 
-export default class Works extends React.Component {
+import { GridList, GridListTile } from '@material-ui/core';
+
+type Tile = {
+  img: string
+  title: string
+  cols?: number
+  rows?: number
+}
+
+type Props = {
+}
+
+type State = {
+  tileData: Tile[]
+}
+
+export default class Works extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      tileData: [
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive', cols: 2, rows: 2 },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive', rows: 2 },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive', rows: 2 },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive' },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive' },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive' },
+        { img: 'https://material-ui.com/static/images/grid-list/olive.jpg', title: 'olive' },
+      ]
+    };
+  }
+
   render() {
     return (
-      <div>制作実績だよ</div>
+      <div className='works__container'>
+        <div className='works__title'>
+          <p className='works__mainTitle'>制作実績</p>
+          <p className='works__subTitle'>Works</p>
+        </div>
+        <div className='works__contents'>
+          <GridList cellHeight={240} cols={4}>
+            {this.state.tileData.map((tile: Tile) => (
+              <GridListTile key={tile.img} cols={tile.cols || 1} rows={tile.rows || 1}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </div>
     );
   }
 }
