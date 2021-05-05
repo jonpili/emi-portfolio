c9bf7991357ff74ca5f33e6faf13fcb5430c9d49
JoeTakasuna <jonpili99@gmail.com>
2021-05-05 19:34:25 +0900

    make first-view style loosely

--- src/pages/FirstView.tsx
+++ src/pages/FirstView.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './FirstView.scss';
+
+import { Container } from '@material-ui/core';
 
 export default class FirstView extends React.Component {
   render() {
     return (
-      <div>FirstViewだよ</div>
+      <div id="first-view" className='firstView__container'>
+        <Container maxWidth="md">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="olive" width="960px"/>
+        </Container>
+      </div>
     );
   }
 }
