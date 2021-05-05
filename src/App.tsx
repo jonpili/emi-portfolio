474492cec5e358a7f9c528d7211719422f4f0e7c
JoeTakasuna <jonpili99@gmail.com>
2021-05-05 15:39:59 +0900

    use scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import Header from './organisms/Header';
 import FirstView from './pages/FirstView';
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
-import './index.css';
+import './reset.scss';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
